1b5f973accb575989099825420590dcd5257feac
KyungBin7 <jkb12377@naver.com>
2025-05-21 22:06:44 +0900

    init

---
+++ .terraform.lock.hcl
@@ -0,0 +1,43 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/local" {
+  version = "2.5.3"
+  hashes = [
+    "h1:MCzg+hs1/ZQ32u56VzJMWP9ONRQPAAqAjuHuzbyshvI=",
+    "zh:284d4b5b572eacd456e605e94372f740f6de27b71b4e1fd49b63745d8ecd4927",
+    "zh:40d9dfc9c549e406b5aab73c023aa485633c1b6b730c933d7bcc2fa67fd1ae6e",
+    "zh:6243509bb208656eb9dc17d3c525c89acdd27f08def427a0dce22d5db90a4c8b",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:885d85869f927853b6fe330e235cd03c337ac3b933b0d9ae827ec32fa1fdcdbf",
+    "zh:bab66af51039bdfcccf85b25fe562cbba2f54f6b3812202f4873ade834ec201d",
+    "zh:c505ff1bf9442a889ac7dca3ac05a8ee6f852e0118dd9a61796a2f6ff4837f09",
+    "zh:d36c0b5770841ddb6eaf0499ba3de48e5d4fc99f4829b6ab66b0fab59b1aaf4f",
+    "zh:ddb6a407c7f3ec63efb4dad5f948b54f7f4434ee1a2607a49680d494b1776fe1",
+    "zh:e0dafdd4500bec23d3ff221e3a9b60621c5273e5df867bc59ef6b7e41f5c91f6",
+    "zh:ece8742fd2882a8fc9d6efd20e2590010d43db386b920b2a9c220cfecc18de47",
+    "zh:f4c6b3eb8f39105004cf720e202f04f57e3578441cfb76ca27611139bc116a82",
+  ]
+}
+
+provider "registry.terraform.io/terraform-provider-openstack/openstack" {
+  version     = "1.54.1"
+  constraints = "~> 1.53"
+  hashes = [
+    "h1:Cqk18+r4bJF/sIusEK9lM0gc841RwsJ8AMhWyiU7lig=",
+    "zh:45ba84df17f94b15af7aab7007241e035dde8a5b46aeb761259d937058a80f71",
+    "zh:493b1deb7be9b600e5b1f5da2a9dfd3bce5df0c6d38090614dbe4ed05ade8441",
+    "zh:53551401fba8c1d5b27a08ee307552b84b1d0c1218f3717a4b766ec701b3e016",
+    "zh:53629bebb48ce5220f7601d776c2ac1485b6c860cb695f150fb716f5be8aa86d",
+    "zh:5a20f32cca767bef70b79bc8ecbd10fec3dc8696183e2d29631aa510947cb70d",
+    "zh:653693f630777e4aa3f410976a5169cf0f2a301516a820b3860de116054ae30a",
+    "zh:70f2d7bd5f5940f4fc3f023a01468890fbd9d704d0256bc65f7c64fb2cbcd4e4",
+    "zh:9cc22af51e5124dd5c2e0f1adefb1b08dcff3138aba9c92961cef36b1641d7aa",
+    "zh:9df45e893f215266159733dbc120809bc3d313188e121532dc6e2d10165e9899",
+    "zh:cb3e240992069cd6160f5b5cbbd50b70948f25bb337a75e780a0648461505d3f",
+    "zh:cb8343c0cf1bf5ca4d060826a8b68e3e5935b4a65974c76ac9c071c5a510e67e",
+    "zh:cc2060f93c66276dff6366b48e3a0e619874e3d939e0d2a39fc6ce10ca91232d",
+    "zh:d495b3051977018696113eded89c2cddfae0570f2adbdf7e9097c189ba41903e",
+    "zh:dfad1be943769780d5e948c06db957ce45f98b057a774964da0b82130c22f139",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,430 @@
+terraform {
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.53"
+    }
+  }
+  # OpenTofu 1.5 이상
+  required_version = ">= 1.5.0"
+}
+
+provider "openstack" {
+  # variables.tf에서 받아온 값 사용
+  cloud    = var.openstack_cloud
+  insecure = var.openstack_insecure
+}
+
+########################
+# 1. 보안그룹
+########################
+
+resource "openstack_networking_secgroup_v2" "allow_icmp_tcp_udp_sg" {
+  name        = "allow-icmp-tcp-udp"
+  description = "Allow all inbound/outbound ICMP, TCP, UDP"
+}
+
+# Ingress ICMP
+resource "openstack_networking_secgroup_rule_v2" "icmp_in" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+}
+
+# Egress ICMP
+resource "openstack_networking_secgroup_rule_v2" "icmp_out" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+}
+
+# Ingress TCP
+resource "openstack_networking_secgroup_rule_v2" "tcp_in" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+}
+
+# Egress TCP
+resource "openstack_networking_secgroup_rule_v2" "tcp_out" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+}
+
+# Ingress UDP
+resource "openstack_networking_secgroup_rule_v2" "udp_in" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+}
+
+# Egress UDP
+resource "openstack_networking_secgroup_rule_v2" "udp_out" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+}
+
+########################
+# 2. k8s-external 네트워크/서브넷
+########################
+
+resource "openstack_networking_network_v2" "k8s_external_net" {
+  name           = var.k8s_external_net_name
+  admin_state_up = true
+}
+
+resource "openstack_networking_subnet_v2" "k8s_external_subnet" {
+  name            = var.k8s_external_subnet_name
+  network_id      = openstack_networking_network_v2.k8s_external_net.id
+  cidr            = var.k8s_external_cidr
+  ip_version      = 4
+  gateway_ip      = var.k8s_external_gateway_ip
+  enable_dhcp     = true
+  dns_nameservers = var.k8s_external_dns
+}
+
+########################
+# 3. k8s-internal 네트워크/서브넷
+########################
+
+resource "openstack_networking_network_v2" "k8s_internal_net" {
+  name           = var.k8s_internal_net_name
+  admin_state_up = true
+}
+
+resource "openstack_networking_subnet_v2" "k8s_internal_subnet" {
+  name       = var.k8s_internal_subnet_name
+  network_id = openstack_networking_network_v2.k8s_internal_net.id
+  cidr       = var.k8s_internal_cidr
+  ip_version = 4
+  # var.k8s_internal_gateway_ip가 빈 문자열("")이면 gateway_ip를 null로 처리
+  gateway_ip  = length(var.k8s_internal_gateway_ip) > 0 ? var.k8s_internal_gateway_ip : null
+  enable_dhcp = var.k8s_enable_dhcp
+}
+
+########################
+# 키 페어
+########################
+
+resource "openstack_compute_keypair_v2" "auto_gen_key" {
+  name = "tofu-key"
+  # 공개키를 입력하지 않으면 Terraform이 자동 생성
+}
+
+# Private Key 출력 (민감데이터)
+output "generated_private_key" {
+  description = "This is the newly generated private key from OpenStack"
+  value       = openstack_compute_keypair_v2.auto_gen_key.private_key
+  sensitive   = true
+}
+
+# Public Key 출력
+output "generated_public_key" {
+  description = "This is the newly generated public key from OpenStack"
+  value       = openstack_compute_keypair_v2.auto_gen_key.public_key
+}
+
+# Private Key를 로컬 파일로 저장
+resource "local_file" "my_private_key_file" {
+  content         = openstack_compute_keypair_v2.auto_gen_key.private_key
+  filename        = "./tofu-key.pem"
+  file_permission = "400"
+}
+
+########################
+# Infra Port들
+########################
+
+data "openstack_networking_network_v2" "infra_net" {
+  name = "infra-net" # 실제 네트워크 이름으로 변경
+}
+
+resource "openstack_networking_port_v2" "infra_port_controller" {
+  name               = "infra-port-controller"
+  network_id         = data.openstack_networking_network_v2.infra_net.id
+  admin_state_up     = true
+  security_group_ids = [openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id]
+}
+
+resource "openstack_networking_port_v2" "infra_port_compute1" {
+  name               = "infra-port-compute1"
+  network_id         = data.openstack_networking_network_v2.infra_net.id
+  admin_state_up     = true
+  security_group_ids = [openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id]
+}
+
+resource "openstack_networking_port_v2" "infra_port_compute2" {
+  name               = "infra-port-compute2"
+  network_id         = data.openstack_networking_network_v2.infra_net.id
+  admin_state_up     = true
+  security_group_ids = [openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id]
+}
+
+resource "openstack_networking_port_v2" "infra_port_storage" {
+  name               = "infra-port-storage"
+  network_id         = data.openstack_networking_network_v2.infra_net.id
+  admin_state_up     = true
+  security_group_ids = [openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id]
+}
+
+resource "openstack_networking_port_v2" "infra_port_ansible_server" {
+  name               = "infra-port-ansible_server"
+  network_id         = data.openstack_networking_network_v2.infra_net.id
+  admin_state_up     = true
+  security_group_ids = [openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id]
+}
+
+########################
+# K8s external / internal Port
+########################
+
+resource "openstack_networking_port_v2" "k8s_external_port_1" {
+  name       = "k8s-external-port-1"
+  network_id = openstack_networking_network_v2.k8s_external_net.id
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.k8s_external_subnet.id
+    ip_address = var.k8s_external_port_1_ip
+  }
+}
+
+resource "openstack_networking_port_v2" "k8s_external_port_2" {
+  name       = "k8s-external-port-2"
+  network_id = openstack_networking_network_v2.k8s_external_net.id
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.k8s_external_subnet.id
+    ip_address = var.k8s_external_port_2_ip
+  }
+}
+
+resource "openstack_networking_port_v2" "k8s_external_port_3" {
+  name       = "k8s-external-port-3"
+  network_id = openstack_networking_network_v2.k8s_external_net.id
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.k8s_external_subnet.id
+    ip_address = var.k8s_external_port_3_ip
+  }
+}
+
+resource "openstack_networking_port_v2" "k8s_internal_port_1" {
+  name       = "k8s_internal_port_1"
+  network_id = openstack_networking_network_v2.k8s_internal_net.id
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.k8s_internal_subnet.id
+    ip_address = var.k8s_internal_port_1_ip
+  }
+}
+
+resource "openstack_networking_port_v2" "k8s_internal_port_2" {
+  name       = "k8s_internal_port_2"
+  network_id = openstack_networking_network_v2.k8s_internal_net.id
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.k8s_internal_subnet.id
+    ip_address = var.k8s_internal_port_2_ip
+  }
+}
+
+resource "openstack_networking_port_v2" "k8s_internal_port_3" {
+  name       = "k8s_internal_port_3"
+  network_id = openstack_networking_network_v2.k8s_internal_net.id
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.k8s_internal_subnet.id
+    ip_address = var.k8s_internal_port_3_ip
+  }
+}
+
+resource "openstack_networking_port_v2" "k8s_internal_port_4" {
+  name       = "k8s_internal_port_4"
+  network_id = openstack_networking_network_v2.k8s_internal_net.id
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.k8s_internal_subnet.id
+    ip_address = var.k8s_internal_port_4_ip
+  }
+}
+
+resource "openstack_networking_port_v2" "k8s_internal_port_5" {
+  name       = "k8s_internal_port_5"
+  network_id = openstack_networking_network_v2.k8s_internal_net.id
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.k8s_internal_subnet.id
+    ip_address = var.k8s_internal_port_5_ip
+  }
+}
+
+########################
+# 1) Ansible Server
+########################
+resource "openstack_compute_instance_v2" "ansible_server" {
+  name        = var.ansible_server_name
+  flavor_name = var.openstack_flavor_name
+  image_id    = var.openstack_image_id
+  key_pair    = openstack_compute_keypair_v2.auto_gen_key.name
+
+  network {
+    port = openstack_networking_port_v2.infra_port_ansible_server.id
+  }
+
+  network {
+    port = openstack_networking_port_v2.k8s_internal_port_1.id
+  }
+
+  network {
+    port = openstack_networking_port_v2.k8s_external_port_1.id
+  }
+
+  user_data = templatefile(
+    "${path.module}/${var.cloud_init_template}",
+    { public_key = openstack_compute_keypair_v2.auto_gen_key.public_key }
+  )
+
+  config_drive = true
+}
+
+########################
+# 2) Controller Node
+########################
+resource "openstack_compute_instance_v2" "controller_node" {
+  name        = var.controller_node_name
+  flavor_name = var.openstack_flavor_name
+  image_id    = var.openstack_image_id
+  key_pair    = openstack_compute_keypair_v2.auto_gen_key.name
+
+  network {
+    port = openstack_networking_port_v2.infra_port_controller.id
+  }
+
+  network {
+    port = openstack_networking_port_v2.k8s_internal_port_2.id
+  }
+
+  user_data = templatefile(var.cloud_init_template, {
+    public_key = openstack_compute_keypair_v2.auto_gen_key.public_key
+  })
+
+  config_drive = true
+}
+
+########################
+# 3) Compute Node1
+########################
+resource "openstack_compute_instance_v2" "compute_node1" {
+  name        = var.compute_node1_name
+  flavor_name = var.openstack_flavor_name
+  image_id    = var.openstack_image_id
+  key_pair    = openstack_compute_keypair_v2.auto_gen_key.name
+
+  network {
+    port = openstack_networking_port_v2.infra_port_compute1.id
+  }
+
+  network {
+    port = openstack_networking_port_v2.k8s_internal_port_3.id
+  }
+
+  network {
+    port = openstack_networking_port_v2.k8s_external_port_2.id
+  }
+
+  user_data = templatefile(var.cloud_init_template, {
+    public_key = openstack_compute_keypair_v2.auto_gen_key.public_key
+  })
+
+  config_drive = true
+}
+
+########################
+# 4) Compute Node2
+########################
+resource "openstack_compute_instance_v2" "compute_node2" {
+  name        = var.compute_node2_name
+  flavor_name = var.openstack_flavor_name
+  image_id    = var.openstack_image_id
+  key_pair    = openstack_compute_keypair_v2.auto_gen_key.name
+
+  network {
+    port = openstack_networking_port_v2.infra_port_compute2.id
+  }
+
+  network {
+    port = openstack_networking_port_v2.k8s_internal_port_4.id
+  }
+
+  network {
+    port = openstack_networking_port_v2.k8s_external_port_3.id
+  }
+
+  user_data = templatefile(var.cloud_init_template, {
+    public_key = openstack_compute_keypair_v2.auto_gen_key.public_key
+  })
+
+  config_drive = true
+}
+
+########################
+# 5) Storage Node
+########################
+resource "openstack_compute_instance_v2" "storage_node" {
+  name        = var.storage_node_name
+  flavor_name = var.openstack_flavor_name
+  image_id    = var.openstack_image_id
+  key_pair    = openstack_compute_keypair_v2.auto_gen_key.name
+
+  network {
+    port = openstack_networking_port_v2.infra_port_storage.id
+  }
+
+  network {
+    port = openstack_networking_port_v2.k8s_internal_port_5.id
+  }
+
+  user_data = templatefile(var.cloud_init_template, {
+    public_key = openstack_compute_keypair_v2.auto_gen_key.public_key
+  })
+
+  config_drive = true
+}
